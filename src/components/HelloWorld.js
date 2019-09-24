cb1ef8726209cc13fdf5d3c3296f1dbc5797db60
Jessica Evans <jme@mail.usf.edu>
2019-09-24 14:52:21 -0400

    added new hello message

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 class HelloWorld extends Component {
   render() {
-    return <h1>Hello, World!</h1>
+    return <h1>Hello, Michelle!</h1>
   }
 }
 
