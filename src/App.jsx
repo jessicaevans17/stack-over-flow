d87cbf1abcdeaaa1c68bf386d26a60870e82324f
Jessica Evans <jme@mail.usf.edu>
2019-09-26 14:18:58 -0400

    before making a branch

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ class App extends Component {
           <nav className="side-nav">
             <a href="#">Home</a>
             <a href="#">Public</a>
-            <div>
+            <div className="side-nav-tab">
               <a href="#">{world} Stack Overflow</a>
               <a href="#">Tags</a>
               <a href="#">Users</a>
@@ -35,49 +35,59 @@ class App extends Component {
             </div>
             <a href="#">Teams</a>
           </nav>
-          <header>
-            <div className="top-part">
-              <h1>All Questions</h1>
-              <a href="#">Ask a Question</a>
-            </div>
-            <div className="bottom-part">
-              <p>18,269,585 questions</p>
-              <div>
-                <a href="#">Newest</a>
-                <a href="#">Active</a>
-                <a href="#">Bountied</a>
-                <a href="#">Unanswered</a>
-                <a href="#">More</a>
+          <div className="main-content">
+            <header>
+              <div className="top-part">
+                <h1>All Questions</h1>
+                <a href="#">Ask a Question</a>
               </div>
-              <a href="#">{cog}Filter</a>
-            </div>
-          </header>
-          <section>
-            <section className="votes-answers">
-              <div className="total-votes">
-                <h2>0</h2>
-                <p>votes</p>
+              <div className="bottom-part">
+                <p>18,269,585 questions</p>
+                <div className="filter-options">
+                  <a className="newest-filter" href="#">
+                    Newest
+                  </a>
+                  <a href="#">Active</a>
+                  <a href="#">Bountied</a>
+                  <a href="#">Unanswered</a>
+                  <a className="more-filter" href="#">
+                    More
+                  </a>
+                  <a className="filter-button" href="#">
+                    {cog}Filter
+                  </a>
+                </div>
               </div>
-              <div className="total-answers">
-                <h2>0</h2>
-                <p>answers</p>
-              </div>
-              <div>
+            </header>
+            <section className="question-preview">
+              <section className="votes-answers">
+                <div className="total-votes">
+                  <h2>0</h2>
+                  <p>votes</p>
+                </div>
+                <div className="total-answers">
+                  <h2>0</h2>
+                  <p>answers</p>
+                </div>
+                <div>
+                  <p>
+                    <span>2</span> views
+                  </p>
+                </div>
+              </section>
+              <section className="question-and-answer">
+                <a href="#">How do you build Stack Overflow in 5 days???</a>
                 <p>
-                  <span>2</span> views
+                  Mark clearly hates us and told us to make stack overflow. How
+                  do you do this??? Lorem ipsum dolor, sit amet consectetur
+                  adipisicing elit. Ipsum molestiae quidem necessitatibus ex
+                  dolorum voluptas perferendis nesciunt quia facilis.
+                  Blanditiis, dolores debitis. Ad, ratione doloribus veniam
+                  accusamus quidem obcaecati itaque!......
                 </p>
-              </div>
+              </section>
             </section>
-            <a href="#">How do you build Stack Overflow in 5 days???</a>
-            <p>
-              Mark clearly hates us and told us to make stack overflow. How do
-              you do this??? Lorem ipsum dolor, sit amet consectetur adipisicing
-              elit. Ipsum molestiae quidem necessitatibus ex dolorum voluptas
-              perferendis nesciunt quia facilis. Blanditiis, dolores debitis.
-              Ad, ratione doloribus veniam accusamus quidem obcaecati
-              itaque!......
-            </p>
-          </section>
+          </div>
         </main>
         <footer>
           <section className="col-1">
